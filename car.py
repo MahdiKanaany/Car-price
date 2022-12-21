e662ccd327eda0f98da5583788ff3876500b71c6
MahdiKanaany <mkanani13882009@gmail.com>
2022-12-21 09:18:37 +0330

    write the files into 'json.txt'

--- car.py
+++ car.py
@@ -1,4 +1,6 @@
 import requests as re
+w = open('json.txt','a')
 token = 'ef8c48f8416a982372028e7aef4d8357'
 url = 'https://sourcearena.ir/api/?token=%s&car=all'%(token)
-print(re.get(url).json())
+response = re.get(url).json()
+w.write(str(response))
