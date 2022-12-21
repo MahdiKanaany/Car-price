83d10456560da40294e124203bea199915148081
MahdiKanaany <mkanani13882009@gmail.com>
2022-12-21 10:03:21 +0330

    read the models

--- car.py
+++ car.py
@@ -1,6 +1,10 @@
 import requests as re
+import json
 w = open('json.txt','a')
 token = 'ef8c48f8416a982372028e7aef4d8357'
 url = 'https://sourcearena.ir/api/?token=%s&car=all'%(token)
 response = re.get(url).json()
 w.write(str(response))
+w.close()
+for item in response:
+    print(item['model'])
